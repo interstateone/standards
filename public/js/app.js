e6a4a5e78765df18b738591b418e9a5239bb72ea
Brandon Evans <brandon@brandonevans.ca>
2012-07-31 13:33:04 -0600

    Move google analytics into layout .erb files

--- public/js/app.js
+++ public/js/app.js
@@ -1280,16 +1280,6 @@
       window.app = new App;
       return window.app.initialize();
     };
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-30914801-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();;
-
     return {
       initialize: initialize
     };
