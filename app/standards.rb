3a71362f73bb796524f221381c31d06d184cb7af
Brandon Evans <brandon@brandonevans.ca>
2012-08-01 01:04:15 -0600

    try heroku settings for rack-cache

--- app/standards.rb
+++ app/standards.rb
@@ -15,8 +15,8 @@ class Standards < Sinatra::Base
   if memcache_servers = ENV["MEMCACHE_SERVERS"]
     use Rack::Cache,
       :verbose => true,
-      :metastore => "memcached://#{memcache_servers}",
-      :entitystore => "memcached://#{memcache_servers}"
+      :metastore => Dalli::Client.new,
+      :entitystore => 'file:tmp/cache/rack/body'
     set :static_cache_control, [:public, {:max_age => 60*60*24*7}]
   end
   use Rack::Deflater
